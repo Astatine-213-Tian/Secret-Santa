296008246cf558a0423b4f9b18152f645512c04b
Teresa Tian <astatine213.tian@gmail.com>
2025-02-21 07:21:04 -0500

    add dialog for joining event

--- src/components/join-event.tsx
+++ src/components/join-event.tsx
@@ -1,28 +1,60 @@
 "use client"
 
+import { useState } from "react"
 import { CirclePlus } from "lucide-react"
 import { toast } from "sonner"
 
 import { joinEvent } from "@/server/actions/event"
 import { Button } from "./ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
+import { Input } from "./ui/input"
 
 export const JoinEvent = () => {
-  // TODO: Remove this
-  const defaultJoinCode = "123456"
+  const [open, setOpen] = useState(false)
 
-  const handleJoinEvent = async () => {
-    const result = await joinEvent({ joinCode: defaultJoinCode })
+  const handleJoinEvent = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.target as HTMLFormElement)
+    const joinCode = formData.get("join-code") as string
+    const result = await joinEvent({ joinCode })
     if (result?.error) {
       toast.error(result.error)
     } else {
       toast.success("Event joined successfully")
+      setOpen(false)
     }
   }
 
   return (
-    <Button variant="outline" onClick={handleJoinEvent}>
-      <CirclePlus className="w-4 h-4" />
-      Join New Event
-    </Button>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <CirclePlus className="w-4 h-4" />
+          Join New Event
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Join Event</DialogTitle>
+          <DialogDescription>
+            Ask your event organizer for the 6-digit join code to join the
+            event.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleJoinEvent} className="space-y-4">
+          <Input placeholder="xxxxxx" name="join-code" maxLength={6} />
+          <Button type="submit" className="w-full">
+            Join Event
+          </Button>
+        </form>
+      </DialogContent>
+    </Dialog>
   )
 }
