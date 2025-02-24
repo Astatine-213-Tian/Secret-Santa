0864fc8be50bf21d5929c28329ebe0819bef43a4
Teresa Tian <astatine213.tian@gmail.com>
2025-02-24 00:45:28 -0500

    clean up

--- src/app/(with-nav)/page.tsx
+++ src/app/(with-nav)/page.tsx
@@ -29,9 +29,7 @@ export default async function Home() {
       <div className="max-w-[2000px] mx-auto px-4 sm:px-6 lg:px-8">
         <CardGrid />
         <div className="flex items-center justify-between mb-6 mt-12">
-          <h2 className="text-xl font-semibold text-gray-900">
-            Events You're Organizing
-          </h2>
+          <h2 className="text-xl font-semibold">Events You're Organizing</h2>
 
           <Link
             href="/event/create"
@@ -48,7 +46,7 @@ export default async function Home() {
         </div>
 
         <div className="flex items-center justify-between mb-6 mt-12">
-          <h2 className="text-xl font-semibold text-gray-900">
+          <h2 className="text-xl font-semibold">
             Events You're Participating In
           </h2>
           <JoinEvent />
