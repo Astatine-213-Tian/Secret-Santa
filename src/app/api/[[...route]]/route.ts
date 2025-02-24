dc40c22311b1a84dfd5a230e5bb52d4521bd01e6
Teresa Tian <astatine213.tian@gmail.com>
2025-02-23 23:52:01 -0500

    chore: remove jstack

--- src/app/api/[[...route]]/route.ts
+++
@@ -1,7 +0,0 @@
-import { handle } from "hono/vercel"
-
-import appRouter from "@/server"
-
-// This route catches all incoming API requests and lets your appRouter handle them.
-export const GET = handle(appRouter.handler)
-export const POST = handle(appRouter.handler)
