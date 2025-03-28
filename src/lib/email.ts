069bc243890db401f7f22f235bc51cdb4ce40121
Teresa Tian <astatine213.tian@gmail.com>
2025-03-27 21:34:01 -0400

    fix email type

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -7,16 +7,17 @@ interface SendEmailProps {
   html: string
 }
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+})
+
 export async function sendEmail({ to, subject, text, html }: SendEmailProps) {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  })
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to,
