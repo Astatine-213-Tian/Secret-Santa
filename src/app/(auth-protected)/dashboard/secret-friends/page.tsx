3d1120dbe20f48f2c72863f0819decda23e7f1fe
Teresa Tian <astatine213.tian@gmail.com>
2025-04-01 00:42:52 -0400

    fix typo

--- src/app/(auth-protected)/dashboard/secret-friends/page.tsx
+++ src/app/(auth-protected)/dashboard/secret-friends/page.tsx
@@ -2,19 +2,19 @@ import {
   fetchUserGiftGivers,
   fetchUserGiftReceivers,
 } from "@/server/queries/event"
-import { AssigmentCard } from "@/components/assigment-card"
+import { AssignmentCard } from "@/components/assignment-card"
 
 export default async function GiftsPage() {
   const receivers = await fetchUserGiftReceivers()
   const givers = await fetchUserGiftGivers()
 
   const receiversUi = receivers.map((r) => (
-    <AssigmentCard key={r.event.id} event={r.event} assignee={r.receiver} />
+    <AssignmentCard key={r.event.id} event={r.event} assignee={r.receiver} />
   ))
 
   // giver might be null => secret
   const giversUi = givers.map((a) => (
-    <AssigmentCard
+    <AssignmentCard
       key={a.event.id}
       event={a.event}
       assignee={a.giver ?? "secret"}
@@ -25,12 +25,12 @@ export default async function GiftsPage() {
     <div className="flex flex-col gap-3">
       <h1 className="text-3xl font-semibold text-gray-800">Secret Friends</h1>
       {!receivers || !givers ? (
-        <div>You have no gift assigments yet. Try joining an event!</div>
+        <div>You have no gift assignments yet. Try joining an event!</div>
       ) : (
         <>
           {/* PEOPLE YOUR ASSIGNED TO */}
           <h2 className="text-2xl font-semibold text-gray-800">
-            My Assigments's
+            My Assignments's
           </h2>
           <div className="grid mt-6 grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 md:gap-8 mb-8">
             {receiversUi}
